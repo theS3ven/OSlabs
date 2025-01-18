d0114bf158aba8cb8ebd3b6c3bec22a2b60d746d
Mete Khan <68829226+theS3ven@users.noreply.github.com>
2025-01-18 04:12:34 +0300

    Update ChildOne.c

--- lr3/src/ChildOne.c
+++ lr3/src/ChildOne.c
@@ -15,6 +15,7 @@ int main() {
 	int fd = shm_open(name, O_RDWR, 0);
     shmp = mmap(NULL, sizeof(shmp), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	sem_wait(&shmp->sem1);
+
 	int len = strlen(shmp->buf);
 	for (int i = 0; i < len; i++) {
 			char c = shmp->buf[i];
@@ -22,11 +23,6 @@ int main() {
 				shmp->buf[i] += 'a' - 'A';
 			}
 		}
-		if (write(STDOUT_FILENO, shmp->buf, len) == -1) {
-			// log_to_file(log_file, "Dolbaeb with pid %d got error during writing", getpid(), buffer);
-		}
-		// log_to_file(log_file, "Dolbaeb with pid %d wrote \'%s\'", getpid(), buffer);
-	// write(log_file, "", 1);
-	// close(log_file);
+    sem_post(&shmp->sem2);
 	return 0;
 }
