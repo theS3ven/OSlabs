67f48561f681a2eaca9484c2dda8a09074aa0cf0
theS3ven <kovalm3t@gmail.com>
2025-01-25 05:12:52 +0300

    lr2 fixes, finally work

--- lr2/src/main.c
+++ lr2/src/main.c
@@ -5,11 +5,14 @@
 #include <stdatomic.h>
 #include <unistd.h>
 
+#define MAX_THREADS 16
+
+
 typedef struct {
     int n;           
-    double **A;        
+    double *a;        
     double *b;          
-    int start, end;     
+    int start;     
 } data;
 
 
@@ -18,33 +21,33 @@ pthread_mutex_t mutex;
 
 void* gauss(void* arg) {
     data* num = (data*)arg;
-    int n = num->n;
-    double **A = num->A;
+    double *a = num->a;
     double *b = num->b;
-
+    // int k = num->start;
+    int n = num->n;
     for (int k = 0; k < n; k++) {
-        for (int i = num->start; i < num->end; i++) {
-            if (i > k) {
-                double factor = A[i][k] / A[k][k];
-                pthread_mutex_lock(&mutex); 
-
-                for (int j = k; j < n; j++) {
-                    A[i][j] -= factor * A[k][j];
+        double p = a[k * n + k];
+        for (int i = k + 1; i < n; i++) {
+                pthread_mutex_lock(&mutex);
+                double l = a[i * n + k] / p;
+                for(int j = k; j < n; j++){
+                    a[i * n + j] -= l * a[k * n + j];
                 }
-                b[i] -= factor * b[k];
+                b[i] -= l * b[k];
                 pthread_mutex_unlock(&mutex);
             }
-        }
     }
     return NULL;
 }
 
-void back_way(int n, double **A, double *b, double *x) {
-    for (int i = n - 1; i >= 0; i--) {
-        x[i] = b[i] / A[i][i];
-        for (int j = i - 1; j >= 0; j--) {
-            b[j] -= A[j][i] * x[i];
+void back_way(int n, double *a, double *b, double *x) {
+    for(int k = n - 1; k >= 0; k--){
+        x[k] = b[k];
+        for(int i = k + 1; i < n; i++){
+            x[k] -= a[k * n + i] * x[i];
         }
+        x[k] /= a[k * n + k];
+        printf("x[%d] = %5.4f\n", k, x[k]);
     }
 }
 
@@ -54,62 +57,56 @@ int main(int argc, char *argv[]){
 		write(STDOUT_FILENO, msg, sizeof(msg));
         exit(EXIT_FAILURE);
     }
-
-    int num = atoi(argv[1]);
-	int NUM_OF_THREADS = atoi(argv[2]);
-
+    
+    int n = atoi(argv[1]);
+	int num_of_threads = atoi(argv[2]);
     if (pthread_mutex_init(&mutex, NULL) != 0) {
         perror("Mutex initialization error\n");
         exit(EXIT_FAILURE);
     }
 
-	double **A = malloc(num * sizeof(double *));
-    double *b = malloc(num * sizeof(double));
-    double *x = malloc(num * sizeof(double));
-    for (int i = 0; i < num; i++) {
-        A[i] = malloc(num * sizeof(double));
-        for (int j = 0; j < num; j++) {
-            A[i][j] = (double)(rand() % 100);  
+	double *a = malloc(sizeof(*a) * n * n);
+    double *b = malloc(sizeof(*b) * n);
+    double *x = malloc(sizeof(*x) * n);
+
+    // создание матрицы
+    for (int i = 0; i < n; i++) {
+        srand(i * (10 + 1));
+        for (int j = 0; j < n; j++) {
+            a[i * n + j] = rand() % 10 + 1;  
         }
-        b[i] = (double)(rand() % 100);
+        b[i] = rand() % 10 + 1;
     }
 
-	pthread_t threads[NUM_OF_THREADS];
-    data thread_data[NUM_OF_THREADS];
+    // отображение матрицы
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%5.2f ", a[i * n + j]);
+        }printf(" | %5.2f\n", b[i]);
+    }
 
-	int chunk_size = num / NUM_OF_THREADS;  
-    for (int i = 0; i < NUM_OF_THREADS; i++) {
-        thread_data[i].n = num;
-        thread_data[i].A = A;
+    pthread_t threads[MAX_THREADS];
+    data thread_data[MAX_THREADS];
+
+    int chunks = n / num_of_threads;
+    for(int i = 0; i < num_of_threads; i++){
+        thread_data[i].n = n;
+        thread_data[i].a = a;
         thread_data[i].b = b;
-        thread_data[i].start = i * chunk_size;
-        thread_data[i].end = (i + 1) * chunk_size;
+        thread_data[i].start = i * chunks;
         pthread_create(&threads[i], NULL, gauss, &thread_data[i]);
     }
 
-	for (int i = 0; i < NUM_OF_THREADS; i++) {
+    for (int i = 0; i < num_of_threads; i++) {
         pthread_join(threads[i], NULL);
     }
 
-    back_way(num, A, b, x);
+    back_way(n, a, b, x);
 
-	const char msg[] = "Matrix solve:\n";
-	write(STDOUT_FILENO, msg, sizeof(msg));
-
-    for (int i = 0; i < num; i++) { 
- 		char tmpbuf[128];
-    	int n = sprintf(tmpbuf, "x[%d] = %f\n", i, x[i]);
-    	write(STDOUT_FILENO, tmpbuf, n);
-	}
-
-    for (int i = 0; i < num; i++) {
-        free(A[i]);
-    }
-    free(A);
+    free(a);
     free(b);
     free(x);
 
     pthread_mutex_destroy(&mutex);
-
     return 0;
 }
